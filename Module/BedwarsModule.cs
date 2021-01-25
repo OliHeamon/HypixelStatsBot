8bf8a885f85b35950c0cae7f8bc0907024741da5
Oliver Heamon <oliverheamon@gmail.com>
2021-01-25 15:11:09 +0000

    logs db count

--- Module/BedwarsModule.cs
+++ Module/BedwarsModule.cs
@@ -19,6 +19,8 @@ namespace HypixelStatsBot.Module
         public BedwarsModule(IServiceProvider services) 
         {
             database = services.GetRequiredService<PlayerStorage>();
+
+            Console.WriteLine("DB Entry Count: " + database.AccountData.Count());
         }
 
         [Command("addentry")]
